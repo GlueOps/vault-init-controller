b3d0b362c4f8bf6fbcaa0e3c7517d68d830d4024
balajisa09 <sabalaji09@gmail.com>
2023-08-06 11:48:27 +0530

    Add healthcheck for vault

--- main.py
+++ main.py
@@ -15,16 +15,21 @@ if __name__ == "__main__":
 
     # Read values from environment variables if available, otherwise use default values
     namespace = os.getenv("NAMESPACE", "glueops-core-vault")
+    vault_sts_name = os.getenv("VAULT_STS_NAME", "vault")
     vault_k8s_service_name = os.getenv("VAULT_K8S_SERVICE_NAME", "vault-internal")
-    sts_name = os.getenv("VAULT_STS_NAME", "vault")
-    reconcile_period = int(os.getenv("RECONCILE_PERIOD", "5"))
     service_port = os.getenv("SERVICE_PORT", "8200")
-    leader_vault_instance = sts_name+"-0"
+    vault_label_selector = os.getenv("VAULT_LABEL","app.kubernetes.io/name=vault")
+    reconcile_period = int(os.getenv("RECONCILE_PERIOD", "10"))
 
-    vaultClient = vault_k8s_utils.VaultManager(namespace,vault_k8s_service_name,service_port,leader_vault_instance)
+    vaultClient = vault_k8s_utils.VaultManager(namespace,vault_sts_name,vault_k8s_service_name,service_port,vault_label_selector)
     # Start the control loop to watch the vault pods
     while(True):
+        logger.info("Reconciling on vault server...")
         vault_pods = vaultClient.get_vault_pods()
+        if(len(vault_pods) == 0):
+            logger.info("No vault pods found with the label"+vault_label_selector+"in "+namespace)
+            time.sleep(reconcile_period)
+            continue
         all_pods_running = True
         for pod in vault_pods:
             if(pod.status.phase == "Running"):
@@ -40,15 +45,16 @@ if __name__ == "__main__":
             logger.info("All vaults pods are up and running")
             # check vault status 
             if(not vaultClient.isVaultIntialized()):
-                vaultClient.initializeVault()
-                
+                vaultClient.initializeVault()    
             else:
                logger.info('Vault is already Initialized')
             for i in range(0,len(vault_pods)):
                #check if each vault server in the cluster is unsealed
-                if(vaultClient.isVaultSealed("https://"+sts_name+"-"+str(i)+"."+vault_k8s_service_name+"."+namespace+":"+service_port+vault_status_url_path)):
-                   vaultClient.vaultUnseal("https://"+sts_name+"-"+str(i)+"."+vault_k8s_service_name+"."+namespace+":"+service_port+vault_unseal_url_path)
+                if(vaultClient.isVaultSealed("https://"+vault_sts_name+"-"+str(i)+"."+vault_k8s_service_name+"."+namespace+":"+service_port+vault_status_url_path)):
+                   vaultClient.vaultUnseal("https://"+vault_sts_name+"-"+str(i)+"."+vault_k8s_service_name+"."+namespace+":"+service_port+vault_unseal_url_path)
                 else:
-                   logger.info("vault-"+str(i)+" is already unsealed")
-                 # TODO: Add vault health check 
+                   logger.info(vault_sts_name+"-"+str(i)+" is already unsealed")
+    
+        if(vaultClient.vaultHealthCheck() != None):
+            logger.info("Vault cluster is up and running")
         time.sleep(reconcile_period)
--- vault_k8s_utils/utils.py
+++ vault_k8s_utils/utils.py
@@ -19,11 +19,12 @@ vault_init_url_path = '/v1/sys/init'
 
 class VaultManager:
 
-  def __init__(self,vault_namespace,vault_k8s_service_name,service_port,leader_vault):
+  def __init__(self,vault_namespace,vault_sts_name,vault_k8s_service_name,service_port,vault_label_selector):
      self.vault_namespace = vault_namespace
      self.vault_k8s_service_name = vault_k8s_service_name
      self.service_port = service_port
-     self.leader_vault = leader_vault
+     self.vault_sts_name = vault_sts_name
+     self.vault_label_selector = vault_label_selector
   
   def get_vault_pods(self):
       try:
@@ -31,7 +32,7 @@ class VaultManager:
           v1 = client.CoreV1Api()
 
           # Get vault pods in the cluster in the vault namespace
-          pods = v1.list_namespaced_pod(self.vault_namespace,label_selector="app.kubernetes.io/name=vault")
+          pods = v1.list_namespaced_pod(self.vault_namespace,label_selector=self.vault_label_selector)
           return pods.items
 
       except Exception as e:
@@ -39,7 +40,7 @@ class VaultManager:
           return []
 
   def isVaultIntialized(self):
-      vault_init_url = "https://"+self.leader_vault+"."+self.vault_k8s_service_name+"."+self.vault_namespace+":"+self.service_port+vault_init_url_path
+      vault_init_url = "https://"+self.vault_sts_name+"-0"+"."+self.vault_k8s_service_name+"."+self.vault_namespace+":"+self.service_port+vault_init_url_path
       response = requests.get(vault_init_url,verify=False)
       return response.json()['initialized']
 
@@ -50,7 +51,7 @@ class VaultManager:
   def initializeVault(self,shares=5, threshold=3):
     logger.info("Starting to initialize vault...")
     payload = '{"secret_shares" : %d, "secret_threshold" : %d}' % (shares, threshold)
-    vault_init_url = "https://"+self.leader_vault+"."+self.vault_k8s_service_name+"."+self.vault_namespace+":"+self.service_port+vault_init_url_path
+    vault_init_url = "https://"+self.vault_sts_name+"-0"+"."+self.vault_k8s_service_name+"."+self.vault_namespace+":"+self.service_port+vault_init_url_path
     if(secret_config.bucketExists):
       try:
         r = requests.put(vault_init_url, data=payload,verify=False)
@@ -87,3 +88,20 @@ class VaultManager:
               r = requests.put(vault_url, data=payload,verify=False)
           except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
               logger.error("ERR: Error connecting to Vault server")
+
+  def vaultHealthCheck(self):
+    try:
+        response = requests.get("https://"+self.vault_k8s_service_name+"."+self.vault_namespace+":"+self.service_port+"/v1/sys/health",verify=False)
+        response_data = response.json()
+        
+        # Check the status code to ensure the request was successful
+        if response.status_code == 200:
+            # Process the response data as needed
+            return response_data
+        else:
+            logger.info("Vault health status. Status code: "+str(response.status_code))
+            return None
+
+    except requests.exceptions.RequestException as e:
+        logger.error("Error occurred:", e)
+        return None
